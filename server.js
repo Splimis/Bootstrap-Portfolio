6495c4d5028213ad161de99deb7acfc0b9a65e95
Splimis <splimis@gmail.com>
2017-11-15 16:20:57 -0500

    building out backend

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
+var methodOverride = require("method-override");
 
 var app = express(); 
 var PORT = process.env.PORT || 3000; 
@@ -11,9 +11,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-require('./App/routing/apiRoutes.js')(app); 
-require('./App/routing/htmlRoutes.js')(app);
+// Serve static content for the app from the 'public' directory
+
+app.use(express.static(process.cwd() + './public'));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// require('./App/routing/apiRoutes.js')(app); 
+require('./routes/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
